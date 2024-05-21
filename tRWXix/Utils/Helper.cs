d0e65ce55a1040ae70e9b32af48214acaa505949
Sergey Makarov <s.makarov@trovent.io>
2024-05-21 06:38:31 -0700

    create tRWXix sub-project

---
+++ tRWXix/Utils/Helper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tRWXix.Utils
+{
+    internal class Helper
+    {
+        internal static void help()
+        {
+            Console.WriteLine("Usage:\n\t.\\tRWXi.exe /trigger /pid=<pid> /address=<hex address /w code> ;");
+        }
+    }
+}
